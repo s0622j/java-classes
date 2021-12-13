bef007620d805e02f229a11a99d70750a89f6194
shengjie <598759654@qq.com>
2021-12-13 22:17:01 +0800

    01-20211213

---
+++ src/main/java/hsp/chapter01_02/Comment02.java
@@ -0,0 +1,13 @@
+package hsp.chapter01_02;
+
+/**
+ * @author  韩顺平
+ * @version  1.0
+ */
+
+public class Comment02 {
+
+    //编写一个main方法
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/java/hsp/chapter01_02/Test.java
@@ -0,0 +1,12 @@
+package hsp.chapter01_02;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.print("书名\t作者\t价格\t销量\n三国\t罗贯中\t120\t1000\n");
+
+    }
+
+
+}
