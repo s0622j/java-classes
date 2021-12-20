347130ba6e585499b5d9e27174fbd46de0f1c4b2
shengjie <598759654@qq.com>
2021-12-21 00:25:41 +0800

    04-20211220

---
+++ src/main/java/hsp/chapter03/StringToBasicDetail15.java
@@ -0,0 +1,12 @@
+package hsp.chapter03;
+
+public class StringToBasicDetail15 {
+    //编写一个main方法
+    public static void main(String[] args) {
+
+        String str = "hello";
+        //转成int
+        int n1 = Integer.parseInt(str);
+        System.out.println(n1);
+    }
+}
